55c54741d6bfe1d55a425c125533879dba233f06
林玥枫 <linyuefeng@codyy.com>
2017-08-23 13:32:14 +0800

    up

--- canvas_draw/lesson-2/index.js
+++ canvas_draw/lesson-2/index.js
@@ -25,4 +25,51 @@ function bezier(){
     ctx.bezierCurveTo(100,0,200,0,200,100);
     ctx.stroke();
 }
-bezier();
+// bezier();
+
+function demo1(){
+    ctx.beginPath();
+    ctx.moveTo(100,400);
+    ctx.lineTo(300,100);
+    ctx.lineTo(400,400);
+    ctx.stroke();
+
+    ctx.strokeStyle = 'red';
+    ctx.beginPath();
+    ctx.moveTo(100,400);
+    ctx.quadraticCurveTo(300, 100, 400, 400);
+    ctx.stroke();
+    
+}
+// demo1();
+
+function demo2(){
+    ctx.beginPath();
+    ctx.moveTo(100,400);
+    ctx.lineTo(200,100);
+    ctx.lineTo(300,130);
+    ctx.lineTo(400,400);
+    ctx.stroke();
+
+
+    ctx.strokeStyle = 'red';
+    ctx.beginPath();
+    ctx.moveTo(100,400);
+    ctx.bezierCurveTo(200,100,300,130,400,400);
+    ctx.stroke();
+    
+}
+// demo2();
+
+function cloud(){
+    ctx.moveTo(100,200);
+    ctx.quadraticCurveTo(100,150,200,150);
+    // ctx.moveTo(200,100);
+    // ctx.quadraticCurveTo(300,100,300,200);
+    // ctx.moveTo(300,200);
+    // ctx.quadraticCurveTo(300,300,200,300);
+    // ctx.moveTo(200,300);
+    // ctx.quadraticCurveTo(100,300,100,200)
+    ctx.stroke();   
+}
+cloud();
