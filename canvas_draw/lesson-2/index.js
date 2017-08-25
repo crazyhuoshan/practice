4c08c9a71f4b556a68135ec5dc7912622acda0cf
林玥枫 <linyuefeng@codyy.com>
2017-08-25 10:18:19 +0800

    up

--- canvas_draw/lesson-2/index.js
+++ canvas_draw/lesson-2/index.js
@@ -25,7 +25,7 @@ function bezier(){
     ctx.bezierCurveTo(100,0,200,0,200,100);
     ctx.stroke();
 }
-// bezier();
+bezier();
 
 function demo1(){
     ctx.beginPath();
@@ -61,15 +61,10 @@ function demo2(){
 }
 // demo2();
 
-function cloud(){
+function demo3(){
     ctx.moveTo(100,200);
-    ctx.quadraticCurveTo(100,150,200,150);
-    // ctx.moveTo(200,100);
-    // ctx.quadraticCurveTo(300,100,300,200);
-    // ctx.moveTo(300,200);
-    // ctx.quadraticCurveTo(300,300,200,300);
-    // ctx.moveTo(200,300);
-    // ctx.quadraticCurveTo(100,300,100,200)
-    ctx.stroke();   
+    ctx.quadraticCurveTo(180,100,260,200);
+    ctx.quadraticCurveTo(180,300,100,200)
+    ctx.stroke();
 }
-cloud();
+// demo3();
