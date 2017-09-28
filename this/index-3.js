4b3b1af0b80c3ca503baa05c3821601aa552f7a0
林玥枫 <linyuefeng@codyy.com>
2017-09-28 15:21:49 +0800

    up

--- this/index-3.js
+++ this/index-3.js
@@ -1,12 +1,26 @@
 /**
  * 其他改变指向的方法
  */
-var qwer = '1111';
-function demo(){
-    var self = this;
-    setTimeout(function(){
-        console.log(self.qwer);
-    },1000);
-};
-demo();
+var other = {
+    test:'1234',
+    fn:function(){
+        var self = this;
+        setTimeout(function(){
+            console.log(self.test);
+        },100)
+    }
+}
+other.fn();
 
+/**
+ * 箭头函数中的this
+ * 箭头函数没有this指向
+ * this指向全局
+ */
+var arrow = {
+    test2:'33333',
+    fn:()=>{
+        console.log(this.test2);
+    }
+}
+arrow.fn();
